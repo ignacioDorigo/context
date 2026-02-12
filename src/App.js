f1e7d6fc5c8081384b63b917c43359aa1c158667
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-02-11 21:16:14 -0300

    Context usado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./App.css";
+import { UsuarioContext } from "./context/UsuarioContext";
 
 export default function App() {
+  const { usuario } = useContext(UsuarioContext);
   return <div>App</div>;
 }
